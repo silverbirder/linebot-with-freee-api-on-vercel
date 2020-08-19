003f084f290569d83a0f21e81f2dc0d37272a276
shibamoto <silverbirder@gmail.com>
2020-08-19 19:39:47 +0900

    😎 Skip the verify token.

--- pages/api/reply.js
+++ pages/api/reply.js
@@ -4,16 +4,23 @@ const client = new line.Client({
     channelSecret: process.env.LINE_CHANNEL_SECRET
 });
 
-export default (req, res) => {
+export default async (req, res) => {
     if (!req.body) {
         res.json({'message': 'no post data'});
         return
     }
-    req.body.events.map(async (event) => {
-        await client.replyMessage(event.replyToken, {
+    const events = req.body.events;
+    for (let i=0; i<events.length; i++) {
+        const event = events[i];
+        const replyToken = event.replyToken;
+        // 検証用Tokenのため、Skipする必要がある。
+        if (replyToken === '00000000000000000000000000000000' || replyToken === 'ffffffffffffffffffffffffffffffff') {
+            continue
+        }
+        await client.replyMessage(replyToken, {
             type: 'text',
             text: 'Hello World!'
         })
-    });
+    }
     res.json({'message': 'replied message'})
 }
