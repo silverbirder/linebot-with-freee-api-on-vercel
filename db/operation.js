0f4c2406aeda2b65e04d2fc17905a7007cc2dcd8
shibamoto <silverbirder@gmail.com>
2020-08-26 07:17:43 +0900

    😎 Fixed bug "OverwriteModelError: Cannot overwrite `Token` model once compiled."

--- db/operation.js
+++ db/operation.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 const {tokenSchema} = require('./schema');
+const Token = mongoose.model('Token', tokenSchema);
 
 exports.findToken = async (userId) => {
-    const Token = mongoose.model('Token', tokenSchema);
     const res = await Token.find({userId: userId});
     return res;
 };
 
 exports.saveToken = async (userId, accessToken, refreshToken) => {
-    const Token = mongoose.model('Token', tokenSchema);
     // TODO: upsert
     // const token = new Token(
     //     {userId: userId},
@@ -21,7 +20,6 @@ exports.saveToken = async (userId, accessToken, refreshToken) => {
 }
 
 exports.deleteToken = async (userId) => {
-    const Token = mongoose.model('Token', tokenSchema);
     await Token.remove({userId: `${userId}`}, function (err) {
         if (err) throw err;
     });
